1f732405eec00f312b1094328db553a42f3eea2c
danaa <danaaaapop@gmail.com>
2019-02-10 10:57:59 +0200

    First commit

---
+++ app/src/main/java/com/example/danaa/exam_10feb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.danaa.exam_10feb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
